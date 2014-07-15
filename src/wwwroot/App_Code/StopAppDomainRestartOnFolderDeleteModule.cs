b8cc42c9ff77281687385edd784167205c8011bb
luchuncheng <mrlucc@126.com>
2014-07-15 17:04:30 +0800

    解决删除文件夹导致网站重启问题

---
+++ src/wwwroot/App_Code/StopAppDomainRestartOnFolderDeleteModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Reflection;
+
+/// <summary> 
+/// Stops the ASP.NET AppDomain being restarted (which clears 
+/// Session state, Cache etc.) whenever a folder is deleted. 
+/// </summary> 
+public class StopAppDomainRestartOnFolderDeleteModule : System.Web.IHttpModule
+{
+	private static bool DisableFCNs = false;
+	public void Init(HttpApplication context)
+	{
+		if (DisableFCNs) return;
+		PropertyInfo p = typeof(HttpRuntime).GetProperty("FileChangesMonitor", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+		object o = p.GetValue(null, null);
+		FieldInfo f = o.GetType().GetField("_dirMonSubdirs", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.IgnoreCase);
+		object monitor = f.GetValue(o);
+		MethodInfo m = monitor.GetType().GetMethod("StopMonitoring", BindingFlags.Instance | BindingFlags.NonPublic);
+		m.Invoke(monitor, new object[] { });
+		DisableFCNs = true;
+	}
+	public void Dispose() { }
+}
